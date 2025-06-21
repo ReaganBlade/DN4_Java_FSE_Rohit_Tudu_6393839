d155b94dc3605185f6ac7ec6ef2729a6da8511a6
Rohit Tudu <rohittudu181@gmail.com>
2025-06-21 16:38:57 +0530

    Week 1 Algo_Data_Struct Financial_Forecasting_Solution Added

---
+++ Week 1/Algorithms_Data_Structures/FinancialForecasting/RegressionModel.java
@@ -0,0 +1,8 @@
+public class RegressionModel {
+    double Slope;
+    double Intercept;
+    RegressionModel (double slope, double intercept){
+        this.Slope = slope;
+        this.Intercept = intercept;
+    }
+}
