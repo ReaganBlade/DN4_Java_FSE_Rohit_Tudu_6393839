79e4599af646cbd1b24ee68f85979fe44cd8db16
Rohit Tudu <rohittudu181@gmail.com>
2025-07-19 13:42:16 +0530

    Added Account and Greet Microservices with Eureka Discovery Server

---
+++ Week 5/Microservices/greet-service/src/main/java/com/cognizant/greet_service/controller/GreetController.java
@@ -0,0 +1,14 @@
+package com.cognizant.greet_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+    
+    @GetMapping("/greet/{user}")
+    private String greetUser(@PathVariable String user){
+        return "Hello " + user;
+    }
+}
