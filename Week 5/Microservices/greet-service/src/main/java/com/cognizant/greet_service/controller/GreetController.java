d6f888ff02c1e8b177e51344d2bb5e92a535036b
Rohit Tudu <rohittudu181@gmail.com>
2025-07-20 13:34:37 +0530

    Refactor: Switch to application.yml and add README files to all services with screenshots

--- Week 5/Microservices/greet-service/src/main/java/com/cognizant/greet_service/controller/GreetController.java
+++ Week 5/Microservices/greet-service/src/main/java/com/cognizant/greet_service/controller/GreetController.java
@@ -1,18 +1,18 @@
 package com.cognizant.greet_service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetController {
     
-    @GetMapping("/greet/{user}")
+    @RequestMapping("/greet/{user}")
     private String greetUser(@PathVariable String user){
         return "Hello " + user;
     }
 
-    @GetMapping("/greet")
+    @RequestMapping("/greet")
     private String greet(){
         return "Hello World";
     }
