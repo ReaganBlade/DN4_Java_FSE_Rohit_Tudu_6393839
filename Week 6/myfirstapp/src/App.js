3a87b30374c3653eaed82fd4c6b13ab65635241a
Rohit Tudu <rohittudu181@gmail.com>
2025-07-27 17:17:48 +0530

    Week 6 React

---
+++ Week 6/myfirstapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
+const App = () => {
+  return (
+    <>
+    <NavBar />
+    <h1 className="heading-class">Welcome to myfirstreact App</h1>
+    </>
+  )
+}
+
+export default App
